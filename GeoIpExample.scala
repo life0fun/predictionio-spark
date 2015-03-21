11b3e80177917a452362a2f59ef707a913a411a2
haijin yan <life0fun@gmail.com>
2015-03-20 22:35:15 -0700

    flatmapwith f(T, A) => U

---
+++ GeoIpExample.scala
@@ -0,0 +1,102 @@
+
+package pandaspark.examples
+
+import scala.math
+import spark.SparkContext
+import spark.SparkContext._
+import spark.SparkFiles;
+import spark.util.Vector
+import au.com.bytecode.opencsv.CSVReader
+import java.util.Random
+import java.io.StringReader
+import java.io.File
+import com.snowplowanalytics.maxmind.geoip.IpGeo
+
+case class DataPoint(x: Vector, y: Double)
+
+// multiple computations
+// 1. map each country to binary feature to see countries covered by data through countries and bcast to all nodes.
+// 2. mapWith for per partition latency estimate for each country.
+// 3. for data that can not be serialize over the wire, create them in partition can cache them.
+// 
+object GeoIpExample {
+
+  def main(args: Array[String]) {
+    if (args.length != 2) {
+      System.err.println("Usage: GeoIpExample <master> <inputfile>")
+      System.exit(1)
+    }
+    val master = args(0)
+    val inputFile = args(1)
+    val iterations = 100
+    val maxMindPath = "GeoLiteCity.dat"
+    val sc = new SparkContext(master, "GeoIpExample", 
+                              System.getenv("SPARK_HOME"),
+                              Seq(System.getenv("JARS")))
+  
+    val invalidLineCounter = sc.accumulator(0)
+    val inFile = sc.textFile(inputFile)  // geo mapping file passed in as
+    // parse data, foreach line, emit Tuple(_, ipAddr)
+    val parsedInput = inFile.flatMap(
+      line => {
+        try {
+          val row = (new CSVReader(new StringReader(line))).readNext()
+          Some((row(0), row.drop(1).map(_.toDouble)))
+        }
+        catch {
+          case _ => {
+            invalidLineCounter += 1
+            None
+          }
+        }
+      })
+    // GeoLiteCity.dat contains snowplowanalytics IpGeo rows.
+    val geoFile = sc.addFile(maxMindPath)
+    
+    // ipCountries <= map (_, ipAddr) => (ipAddr, countryCode)
+    // mapWith (constructA: _ => IpGeo) (f: (T, A) => U)
+    val ipCountries = parsedInput.flatMapWith
+      (_ => IpGeo(dbFile = SparkFiles.get(maxMindPath)))  // per partition constructA pass to map (f: (T, A) => U)
+      ((pair, ipGeo) => {  // map (f: (T, A) => U) takes additional param constructA per partition
+        //getLocation gives back an option so we use flatMap to only output if it's a some type
+        ipGeo.getLocation(pair._1).map(
+          c => 
+            (pair._1, c.countryCode)).toSeq
+      })
+    ipCountries.cache()
+    
+    val countries = ipCountries.values.distinct().collect()
+    val countriesBc = sc.broadcast(countries)
+    // this RDD is (ipAddr, 0/1)
+    val countriesSignal = ipCountries.mapValues(
+      // if our data contains the country, set flag to 1 means we cover the country.
+      country =>  // lookup all countriesBc, if row of ipCountries in countriesBc, emit 1.
+        countriesBc.value.map(
+          s => if (country == s) 1
+               else 0 
+        )
+    )
+    // dataPoints is 2 column from joining 
+    val dataPoints = parsedInput.join(countriesSignal).map(
+      input => {
+        input._2 match {
+          case (countryData, originalData) =>
+            DataPoint(new Vector(countryData++originalData.slice(1,originalData.size-2)),
+                      originalData(originalData.size-1))
+        } 
+      }
+    )
+    countriesSignal.cache()
+    dataPoints.cache()
+    
+    val rand = new Random(53)
+    var w = Vector(dataPoints.first.x.length, _ => rand.nextDouble)
+    for (i <- 1 to iterations) {
+      val gradient = dataPoints.map(
+        p => (1 / (1 + math.exp(-p.y*(w dot p.x))) - 1) * p.y * p.x).reduce(_ + _)
+
+      w -= gradient
+    }
+    println("Final w: "+w)
+  }
+}
